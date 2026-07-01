6dc07d9f809dae3f46fc7512b304adf42f4081b5
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-07-01 14:59:43 +0200

    feat: update VideoListView to use APIView and return serialized video list with request context

--- video_app/api/urls.py
+++ video_app/api/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from .views import HLSManifestView, HLSSegmentView, VideoListView
@@ -15,3 +17,6 @@ urlpatterns = [
         name="hls_segment",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- video_app/api/views.py
+++ video_app/api/views.py
@@ -1,6 +1,8 @@
 from django.http import FileResponse, Http404
+from rest_framework import status
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from video_app.api.authentication import CookieJWTAuthentication
@@ -10,11 +12,17 @@ from video_app.models import Video
 from .utils import get_hls_manifest_file, get_hls_segment_file
 
 
-class VideoListView(ListAPIView):
-    queryset = Video.objects.all()
-    serializer_class = VideoSerializer
+class VideoListView(APIView):
+    """API endpoint to retrieve all available videos ordered by creation date DESC."""
+
     authentication_classes = [CookieJWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """Return serialized video list sorted newest first with request context."""
+        videos = Video.objects.all()
+        serializer = VideoSerializer(videos, many=True, context={"request": request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class HLSManifestView(APIView):
