1cfb70e8fdcb2842d3c934e541e1eaea7bdd1cf5
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-07-02 15:33:38 +0200

    refactor: remove unused ListAPIView import from views.py

--- video_app/api/views.py
+++ video_app/api/views.py
@@ -1,6 +1,5 @@
 from django.http import FileResponse, Http404
 from rest_framework import status
-from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
