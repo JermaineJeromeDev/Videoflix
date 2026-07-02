8f676fe086c1826d60591c74e5c82fa541074e83
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-07-02 15:20:32 +0200

    feat: update video_app configuration to use AppConfig and ensure signals are imported on ready

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "auth_app",
-    "video_app",
+    "video_app.apps.VideoAppConfig",
 ]
 
 MIDDLEWARE = [
