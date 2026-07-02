953b98d99ca5312361843b00ae614d76ac5975d2
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-07-02 15:43:22 +0200

    refactor: improve docstrings for VideoAppConfig class in apps.py

--- video_app/apps.py
+++ video_app/apps.py
@@ -2,7 +2,11 @@ from django.apps import AppConfig
 
 
 class VideoAppConfig(AppConfig):
+    """Configuration for the video_app application catalog."""
+
     name = "video_app"
+
+    """Override the ready method to import signals when the app is ready."""
 
     def ready(self):
         from . import signals  # noqa: F401
